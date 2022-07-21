4ce14c3210c0f503756b42371a9241af50a80c7f
Edmur <edmurneves@gmail.com>
2022-07-21 09:34:27 -0300

    Importação do middleware de erro para a aplicação.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 require('express-async-errors');
 const express = require('express');
 
+const errorMiddleware = require('../src/middleware/error');
+
 const app = express();
 
 app.use(express.json());
+
+app.use(errorMiddleware);
 
 
 module.exports = app;
