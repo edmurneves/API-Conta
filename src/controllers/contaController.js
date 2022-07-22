1d4821a37d188708f9fd662a21c1b1673dad2b2c
Edmur <edmurneves@gmail.com>
2022-07-22 10:28:17 -0300

    Criado em contaController a rota /saque.

--- src/controllers/contaController.js
+++ src/controllers/contaController.js
@@ -1,23 +1,21 @@
 const express = require('express');
+const { checkSaqueDeposito } = require('../middleware/checkSaqueDeposito');
 const contaService = require('../services/contaService');
 
 const contaRouter = express.Router();
 
 contaRouter.get('/:id', async (req, res) => {
     const { id } = req.params;    
-    const [rows] = await contaService.getById(id);
-
-    if (rows.length === 0) {
-        return res.status(404).json({message: 'Conta inexistente'});
-    }
+    const rows = await contaService.getById(id);   
     
     res.status(200).json(rows[0]);    
 });
 
-contaRouter.post('/saque', async (req, res) => {
-    const { codCliente, valor } = req.body;
-    console.log(codCliente);
-    console.log(valor);
+contaRouter.post('/saque', checkSaqueDeposito, async (req, res) => {   
+    
+    const newSaldo = await contaService.debit(req.body);
+    
+    return res.status(200).json({message: `Saque efetuado com sucesso!, Seu saldo atual é de e ${newSaldo} Reais`});
 });
 
 module.exports = contaRouter;
