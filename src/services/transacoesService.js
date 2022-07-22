019ebe8976870e1369a504ca6d8471a6a103dc4b
Edmur <edmurneves@gmail.com>
2022-07-22 18:02:10 -0300

    Criado o metodo getByClienteId para a services de transacoes.

--- src/services/transacoesService.js
+++ src/services/transacoesService.js
@@ -4,4 +4,17 @@ const getAll = () => {
     return transacoesModel.getAll();
 }
 
-module.exports = { getAll };
+const getByClientId = async (id) => {
+    const rows = await transacoesModel.getByClientId(id);
+    console.log(rows);    
+
+    if (rows.length === 0) {
+        const error = {status:404, message: 'Este cliente não possui ações!' };
+        throw error;        
+    }
+
+    return rows;
+
+}
+
+module.exports = { getAll, getByClientId };
