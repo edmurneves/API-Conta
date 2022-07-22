edc69a9e7aca525a2a9560aaa922e88ed466aa8a
Edmur <edmurneves@gmail.com>
2022-07-22 10:29:41 -0300

    Criado o contaService para efetuar as regras de negócio solicitadas.

--- src/services/contaService.js
+++ src/services/contaService.js
@@ -1,7 +1,35 @@
 const contaModel = require('../models/contaModel');
 
 const getById = async (id) => {
-    return contaModel.getById(id);
+    const [rows] = await contaModel.getById(id);
+
+    if (rows.length === 0) {
+        const error = {status:404, message: 'Conta inexistente' };
+        throw error;        
+    }
+    return rows;
 }
 
-module.exports = { getById };
+const debit = async ({ codCliente, valor }) => {
+    const conta = await getById(codCliente);
+    const saldo = Number(conta[0].saldo);
+    
+    if (saldo < valor) {
+        const error = {status: 400, message:'Saldo insuficiente para saque'};
+        throw error;
+    }
+
+    let newSaldo = saldo - valor;
+
+    const result = await contaModel.updateSaldo(codCliente, newSaldo);
+
+    if (result.affectedRows === 0) {
+        const error = {status: 400, message: 'Erro ao processar o saque, tente novamente mais tarde.'};
+        throw error;
+    }
+    
+    return newSaldo;   
+
+};
+
+module.exports = { getById, debit };
