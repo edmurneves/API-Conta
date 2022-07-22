f8fe019625c11f604a4e8adb6ac3ae0fa470aa23
Edmur <edmurneves@gmail.com>
2022-07-22 11:26:06 -0300

    Criado o método credit confome as regras de negócio

--- src/services/contaService.js
+++ src/services/contaService.js
@@ -31,5 +31,21 @@ const debit = async ({ codCliente, valor }) => {
     return newSaldo;   
 
 };
+const credit = async ({ codCliente, valor }) => {
+    const conta = await getById(codCliente);
+    const saldo = Number(conta[0].saldo);
 
-module.exports = { getById, debit };
+    let newSaldo = saldo + valor;
+
+    const result = await contaModel.updateSaldo(codCliente, newSaldo);
+
+    if (result.affectedRows === 0) {
+        const error = {status: 400, message: 'Erro ao processar o deposito, tente novamente mais tarde.'};
+        throw error;
+    }
+
+    return newSaldo;  
+
+}
+
+module.exports = { getById, debit, credit };
