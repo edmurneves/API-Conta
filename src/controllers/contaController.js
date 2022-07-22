e8eb4f7065da27fbc273f593b4748d4bb0ef6f85
Edmur <edmurneves@gmail.com>
2022-07-22 11:25:10 -0300

    Criada a rota /deposito em contaController

--- src/controllers/contaController.js
+++ src/controllers/contaController.js
@@ -5,7 +5,8 @@ const contaService = require('../services/contaService');
 const contaRouter = express.Router();
 
 contaRouter.get('/:id', async (req, res) => {
-    const { id } = req.params;    
+    const { id } = req.params;
+        
     const rows = await contaService.getById(id);   
     
     res.status(200).json(rows[0]);    
@@ -17,5 +18,13 @@ contaRouter.post('/saque', checkSaqueDeposito, async (req, res) => {
     
     return res.status(200).json({message: `Saque efetuado com sucesso!, Seu saldo atual é de e ${newSaldo} Reais`});
 });
+
+contaRouter.post('/deposito', checkSaqueDeposito, async (req, res) => {   
+    
+    const newSaldo = await contaService.credit(req.body);
+    
+    return res.status(200).json({message: `Deposito efetuado com sucesso!, Seu saldo atual é de e ${newSaldo} Reais`});
+});
+
 
 module.exports = contaRouter;
