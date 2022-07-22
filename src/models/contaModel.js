7d761d7ab9b3bfcc4fcb662090eaee2e40878e14
Edmur <edmurneves@gmail.com>
2022-07-22 10:30:42 -0300

    Criado o método updateSaldo para atualizar o saldo do cliente no BD.

--- src/models/contaModel.js
+++ src/models/contaModel.js
@@ -3,5 +3,10 @@ const connection = require('../db');
 const getById = (id) => connection
 .execute('SELECT codCliente, saldo FROM desafioXP.clientes WHERE codCliente = ?', [id]);
 
+const updateSaldo = async (id, valor) => {
+    const [result] = await connection
+    .execute('Update desafioXP.clientes SET saldo = ? WHERE codCliente = ?;', [valor, id]);        
+    return result;
+};
 
-module.exports = { getById };
+module.exports = { getById, updateSaldo };
