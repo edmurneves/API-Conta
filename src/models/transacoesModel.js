e33031fe93308cf43ca96260efe3344650dd46e0
Edmur <edmurneves@gmail.com>
2022-07-22 18:02:55 -0300

    Criada a query do método getClienteById.

--- src/models/transacoesModel.js
+++ src/models/transacoesModel.js
@@ -2,4 +2,16 @@ const connection = require('../db');
 
 const getAll = () => connection.execute('SELECT * FROM desafioXP.transacoes');
 
-module.exports = { getAll };
+const getByClientId = async (id) => {
+
+    const query = `SELECT t.codCliente, t.codAtivo, t.qtde as qtdeAtivo, a.valor AS valor
+    FROM desafioXP.transacoes AS t
+    INNER JOIN desafioXP.ativos AS a ON t.codAtivo = a.codAtivo
+    WHERE codCliente = ?;`
+    
+    const [result] = await connection.execute(query, [id]);
+        
+    return result;
+}
+
+module.exports = { getAll, getByClientId };
