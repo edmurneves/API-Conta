7dad017f5ee7e261fc64c1aff06db3b83785e8db
Edmur <edmurneves@gmail.com>
2022-07-22 18:00:43 -0300

    Criado o método de getById de Services para a rota /ativos.

--- src/services/ativosService.js
+++ src/services/ativosService.js
@@ -4,4 +4,14 @@ const getAll = () => {
     return ativosModel.getAll();
 }
 
-module.exports = { getAll };
+const getById = async (id) => {
+    const [rows] = await ativosModel.getById(id);
+
+    if (rows.length === 0) {
+        const error = {status:404, message: 'Ativo inexistente' };
+        throw error;        
+    }
+    return rows;
+}
+
+module.exports = { getAll, getById };
