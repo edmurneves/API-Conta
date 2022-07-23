2ca924e694983e8cd2c476fe9eb5a97fb908912e
Edmur <edmurneves@gmail.com>
2022-07-23 12:17:36 -0300

    Criada a model para atualização da quantidade de ativos da corretora após venda.

--- src/models/ativosModel.js
+++ src/models/ativosModel.js
@@ -5,4 +5,11 @@ const getAll = () => connection.execute('SELECT * FROM desafioXP.ativos');
 const getById = (id) => connection
 .execute('SELECT * FROM desafioXP.ativos WHERE codAtivo = ?', [id]);
 
-module.exports = { getAll, getById };
+const updateAtivosQtde = async (codAtivo, qtde) => {
+    const query = `UPDATE desafioXP.ativos SET qtd = ? WHERE codAtivo = ?;`
+    const [result] = await connection.execute(query, [ qtde, codAtivo]);
+
+    return result;
+}
+
+module.exports = { getAll, getById, updateAtivosQtde };
